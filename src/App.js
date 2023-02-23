acf687706efc32be036a678b6c8749c7b0badfb3
bbyik-k <kby00517@naver.com>
2023-02-23 23:26:17 +0900

    Add DarkMode Context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
@@ -15,7 +16,7 @@ function App() {
         onFilterChange={setFilter}
       ></Header>
       <TodoList filter={filter}></TodoList>
-    </>
+    </DarkModeProvider>
   );
 }
 
---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // setDarkMode((prev) => !prev);
+    updateDarkMode(!darkMode);
+  };
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export const useDarkMode = () => useContext(DarkModeContext);
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
